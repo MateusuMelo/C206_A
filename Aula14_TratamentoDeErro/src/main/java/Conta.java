1e2ad85261332ab534a373dcec2642a7f2635fdb
MateusuMelo <mateus.c@ges.inatel.br>
2023-05-09 13:32:30 -0300

    aula 14 tratamento de erro

---
+++ Aula14_TratamentoDeErro/src/main/java/Conta.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Conta {
+    double saldo;
+    double limite;
+    HashSet<Cliente>clientes = new HashSet<>();
+
+    public Conta(double saldo, double limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+    void mostraInfo(){
+        System.out.println("Clientes : ");
+        for (int i = 0;i<clientes.size();i++){
+            try {
+                clientes.contains(i);
+
+            }catch (NullPointerException err){
+                System.out.println("Erro: " + err);
+            }
+        }
+        System.out.println("saldo: " + saldo);
+        System.out.println("limite: " + limite);
+
+    }
+}
